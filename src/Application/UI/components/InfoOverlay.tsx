8282bcb2b7fc2bdbf2390858cf5fbef344727641
zieee2025 <appy98834@gmail.com>
2025-12-14 12:28:14 +0800

    Update NAME_TEXT and TITLE_TEXT in InfoOverlay

--- src/Application/UI/components/InfoOverlay.tsx
+++ src/Application/UI/components/InfoOverlay.tsx
@@ -6,8 +6,8 @@ interface InfoOverlayProps {
     visible: boolean;
 }
 
-const NAME_TEXT = 'Henry Heffernan';
-const TITLE_TEXT = 'Software Engineer';
+const NAME_TEXT = 'Fritzie Jose';
+const TITLE_TEXT = 'Civil Engineering Student';
 const MULTIPLIER = 1;
 
 const InfoOverlay: React.FC<InfoOverlayProps> = ({ visible }) => {
